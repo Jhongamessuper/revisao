da4d4f616636b09faf90287fe4397344def26b48
Jhon Lenon <Jhongamessuper564email@gmail.com>
2022-10-06 11:47:49 -0300

    Teste

---
+++ components/Footer/index.js
@@ -0,0 +1,10 @@
+import styles from "./styles.module.css"
+
+export default function Header() {
+    return (
+        <>
+        <h1>Header Teste</h1>
+        <p className={styles.footer}>Header Teste</p>
+        </>
+    )
+}
